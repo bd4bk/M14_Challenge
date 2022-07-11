d26298f0bd9d020966c4409321999e10768f5c66
Brennan Danek <Brennan@Brennans-MacBook-Air.local>
2022-07-11 10:49:42 -0400

    added our package

---
+++ our_package/jabber.py
@@ -0,0 +1,12 @@
+def banana_peels(n):
+
+    """
+    This function tells you a funny joke based on a number you give it
+    params: int
+    return: int
+    """
+    
+    print("I slipped on " + str(n) + " banana peels!")
+    return n + 1
+
+
