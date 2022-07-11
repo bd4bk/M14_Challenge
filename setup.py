eb5a35eacdb8e793aa29e6557b74aba21c57cd04
Brennan Danek <Brennan@Brennans-MacBook-Air.local>
2022-07-11 10:48:37 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = "jabber",
+     version = "1.0",
+     description = "A meaningful demonstration of our package",
+     url = "https://github.com/bd4bk/M14_Challenge",
+     author = "Brennan Danek and David Siamon",
+     author_email = "bd4bk@virginia.edu",
+     license = "MIT",
+     packages = ["our_package"])
